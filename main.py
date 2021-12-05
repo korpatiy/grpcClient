85efa31abef945940edcb0ef9d5b28d756885143
Vyacheslav Tretyakov <tretiakovslava@gmail.com>
2021-12-05 18:01:41 +0500

    upd

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import grpc
+import sqlite3
+import json
 
-import MigrationService_pb2
-import MigrationService_pb2_grpc
+from proto import MigrationService_pb2_grpc, MigrationService_pb2
+
+
+def get_data():
+    conn = sqlite3.connect('./biathlon-lite.db')
+    conn.row_factory = sqlite3.Row
+    db = conn.cursor()
+    rows = db.execute('SELECT * from t_biathlon').fetchall()
+    conn.commit()
+    conn.close()
+    return json.dumps([dict(ix) for ix in rows])
 
 
 def run():
-    channel = grpc.insecure_channel('10.242.0.2:9092')
+    with open('server.crt', 'rb') as f:
+        creds = grpc.ssl_channel_credentials(root_certificates=f.read())
+    channel = grpc.secure_channel('10.242.0.1:9092', credentials=creds)
     stub = MigrationService_pb2_grpc.MigrationGRPCServiceStub(channel)
-    response = stub.migrateData(MigrationService_pb2.Request(message='12'))
+    response = stub.migrateData(MigrationService_pb2.Request(message=get_data()))
     print("From Server: " + response.message)
     channel.close()
 
